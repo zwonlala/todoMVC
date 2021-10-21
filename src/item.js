0ea7313b7ad4254041e780b8e50a64fb66895666
zwonlala <s26788761@naver.com>
2021-10-21 21:21:37 +0900

    Add item.js

---
+++ src/item.js
@@ -0,0 +1,20 @@
+export var Item;
+export var ItemList;
+
+const Empty = {
+    Record: {}
+};
+
+export var EmptyItemQuery;
+
+export const emptyItemQuery = Empty.Record;
+
+export var ItemQuery;
+export var ItemUpdate;
+
+/**
+ * 궁금한 거
+ *
+ * 해당 파일을 봐도 무엇을 하려는 코드인지 감이 잘 안잡힌다. 해당 코드 사용하는 부분을 봐야할 듯
+ *
+ */
