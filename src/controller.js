643293685376d6d01a6e04272855dbbd11c646ef
zwonlala <s26788761@naver.com>
2021-10-28 21:37:46 +0900

    Add controller.js

---
+++ src/controller.js
@@ -0,0 +1,107 @@
+import { emptyItemQuery } from './item';
+import Store from './store';
+import View from './view';
+
+export default class Controller {
+    constructor(store, view) {
+        this.store = store;
+        this.view = view;
+
+        view.bindAddItem(this.addItem.bind(this));
+        view.bindEditItemSave(this.editItemSave.bind(this));
+        view.bindEditItemCancel(this.editItemCancel.bind(this));
+        view.bindRemoveItem(this.removeItem.bind(this))       ;
+        view.bindToggleItem((id, completed) => {
+            this.toggleCompleted(id, completed);
+            this._filter();
+        });
+        view.bindRemoveCompleted(this.removeCompletedItems.bind(this));
+        view.bindToggleAll(this.toggleAll.bind(this));
+
+        this._activeRoute = '';
+        this._lastActiveRoute = null;
+    }
+
+    setView(raw) {
+        const route = raw.replace(/^#\//, '');
+        this._activeRoute = route;
+        this._filter();
+        this.view.updateFilterButtons(route);
+    }
+
+    addItem(title) {
+        this.store.insert({
+            id: Date.now(),
+            title,
+            completed: false
+        }, () => {
+            this.view.clearNewTodo();
+            this._filter(true);
+        });
+    }
+
+    editItemSave(id, title) {
+        if (title.length) {
+            this.store.update({id, title}, () => {
+                this.view.editItemDone(id, title);
+            });
+        } else {
+            this.removeItem(id);
+        }
+    }
+
+    editItemCancel(id) {
+        this.store.find({id}, data => {
+            const title = data[0].title;
+            this.view.editItemDone(id, title);
+        });
+    }
+
+    removeItem(id) {
+        this.store.remove({id}, () => {
+            this._filter();
+            this.view.removeItem(id);
+        });
+    }
+
+    removeCompletedItems() {
+        this.store.remove({completed: true}, this._filter.bind(this));
+    }
+
+    toggleCompleted(id, completed) {
+        this.store.update({id, completed}, () => {
+            this.view.setItemCompleted(id, completed);
+        });
+    }
+
+    toggleAll(completed) {
+        this.store.find({completed: !completed}, data => {
+            for (let {id} of data) {
+                this.toggleCompleted(id, completed);
+            }
+        });
+        this._filter();
+    }
+
+    _filter(force) {
+        const route = this._activeRoute;
+
+        if (force || this._lastActiveRoute !== '' || this._lastActiveRoute !== route) {
+            this.store.find({
+                '': emptyItemQuery,
+                'active': {completed: false},
+                'completed': {completed: true}
+            }[route], this.view.showItems.bind(this.view));
+        }
+
+        this.store.count((total, active, completed) => {
+            this.view.setItemsLeft(active);
+            this.view.setClearCompletedButtonVisibility(completed);
+
+            this.view.setCompleteAllCheckbox(completed === total);
+            this.view.setMainVisibility(total);
+        });
+
+        this._lastActiveRoute = route;
+    }
+}
